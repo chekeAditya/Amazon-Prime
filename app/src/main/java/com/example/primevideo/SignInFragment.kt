f373366fd653a12c7451a5cf159428ab8aedfde4
Prachip9767 <prachip293@gmail.com>
2021-08-24 23:50:18 +0530

    Prachi update

--- app/src/main/java/com/example/primevideo/SignInFragment.kt
+++ app/src/main/java/com/example/primevideo/SignInFragment.kt
@@ -1,21 +1,19 @@
 package com.example.primevideo
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_sign_in.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignInFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignInFragment : Fragment(R.layout.fragment_sign_in) {
-
+    private lateinit var navController: NavController
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        btnCreateAccount.setOnClickListener {
+        navController.navigate(R.id.action_signInFragment_to_createAccount)
+        }
+    }
 }
