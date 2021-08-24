05f807dc31648a2bd52a284bafbc48d979567fc7
AdityaCheke291 <adityacheke4@gmail.com>
2021-08-24 10:54:06 +0530

    Tabs Created

---
+++ app/src/main/java/com/example/primevideo/Fragments/MoviesFragment.kt
@@ -0,0 +1,12 @@
+package com.example.primevideo.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.primevideo.R
+
+class MoviesFragment : Fragment(R.layout.fragment_movies) {
+
+}
