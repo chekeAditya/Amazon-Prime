3e8a68c64bb8e60e9810fd2e1efa15018a4ab582
Prachip9767 <prachip293@gmail.com>
2021-08-25 18:26:55 +0530

    Prachi update

--- app/src/main/java/com/example/primevideo/Fragments/videoViewFragment.kt
+++ app/src/main/java/com/example/primevideo/Fragments/videoViewFragment.kt
@@ -1,10 +1,6 @@
 package com.example.primevideo.Fragments
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.primevideo.R
 
 class videoViewFragment : Fragment(R.layout.fragment_video_view) {
