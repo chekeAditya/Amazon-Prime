3e79516083afca0bf509231dd3980217cd11f4f5
sudararshan <sudarshanprasad18@gmail.com>
2021-08-25 15:07:48 +0530

    hey kids pick

--- app/src/main/java/com/example/primevideo/Fragments/KidsFragment.kt
+++ app/src/main/java/com/example/primevideo/Fragments/KidsFragment.kt
@@ -2,19 +2,32 @@ package com.example.primevideo.Fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.primevideo.Adapters.KidsPickYouAdapter
 import com.example.primevideo.Adapters.MySliderImageAdapter
+import com.example.primevideo.Model.KidsPickYouModel
+import com.example.primevideo.Model.KidsPickYouResult
+import com.example.primevideo.Network.ApiClient
+import com.example.primevideo.Network.Network
 import com.example.primevideo.R
 import com.smarteist.autoimageslider.SliderView
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.imageSlider
 import kotlinx.android.synthetic.main.fragment_kids.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class KidsFragment : Fragment(R.layout.fragment_kids) {
+
+    private lateinit var listOfKidsPickYou: List<KidsPickYouResult>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         imageSliderView()
+        kidsPickYou()
     }
 
 
@@ -39,4 +52,34 @@ class KidsFragment : Fragment(R.layout.fragment_kids) {
         imageSlider.isAutoCycle = true
         imageSlider.startAutoCycle()
     }
+
+    private fun kidsPickYou() {
+        var apiClient = Network.getInstance().create(ApiClient::class.java)
+        apiClient.getKidsPickYou()
+            .enqueue(object : Callback<KidsPickYouModel> {
+                override fun onResponse(
+                    call: Call<KidsPickYouModel>,
+                    response: Response<KidsPickYouModel>
+                ) {
+                    response.body()?.run {
+                        listOfKidsPickYou = kidsPickYouResults
+                        setAdapter()
+                    }
+                }
+
+                override fun onFailure(call: Call<KidsPickYouModel>, t: Throwable) {
+                    Toast.makeText(context, "Failure" + t.message, Toast.LENGTH_LONG).show()
+                }
+
+            })
+    }
+
+    private fun setAdapter() {
+        var linearLayoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,false)
+        val kidsPickYouAdapter = KidsPickYouAdapter(listOfKidsPickYou)
+        rvKidsPickYou.adapter = kidsPickYouAdapter
+        rvKidsPickYou.layoutManager = linearLayoutManager
+    }
+
+
 }
---
+++ app/src/main/java/com/example/primevideo/Model/KidsPickYouModel.kt
@@ -0,0 +1,15 @@
+package com.example.primevideo.Model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class KidsPickYouModel(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val kidsPickYouResults: List<KidsPickYouResult>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
