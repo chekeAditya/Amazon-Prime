c72a7abae72124fb9549e457a6039178024bc3a0
sudararshan <sudarshanprasad18@gmail.com>
2021-08-27 18:21:18 +0530

    pushing to check ...... issue solved sorry

--- app/src/main/java/com/example/primevideo/Fragments/kidsandexpand.kt
+++ app/src/main/java/com/example/primevideo/Fragments/kidsandexpand.kt
@@ -6,33 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentResultListener
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.primevideo.Adapters.InidanToonsAdaptor
 import com.example.primevideo.Adapters.kidsandfamilytvexpansionAdaptor
 import com.example.primevideo.Model.Kids.kidsandfamily.IndianToon
 import com.example.primevideo.Model.Kids.kidsandfamily.IndianToonsModel
 import com.example.primevideo.Network.ApiClient
 import com.example.primevideo.Network.Network
 import com.example.primevideo.R
-import kotlinx.android.synthetic.main.fragment_kids.*
-import kotlinx.android.synthetic.main.fragment_kids.rvIndianToons
-import kotlinx.android.synthetic.main.fragment_kids_expand.*
-import kotlinx.android.synthetic.main.item_grid_layout.*
+import kotlinx.android.synthetic.main.fragment_kidsandexpand.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-
-class KidsExpandFragment : Fragment(R.layout.fragment_kids_expand)  {
+class kidsandexpand : Fragment(R.layout.fragment_kidsandexpand) {
 
     private lateinit var indianToon: List<IndianToon>
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         parentFragmentManager.setFragmentResultListener(
-            "Actionname",
+            "kidsandfamilytv",
             this,
             FragmentResultListener() { s: String, bundle: Bundle ->
 
@@ -44,6 +39,7 @@ class KidsExpandFragment : Fragment(R.layout.fragment_kids_expand)  {
 
     private fun kidsTv() {
         val apiClient4 = Network.getInstance().create(ApiClient::class.java)
+
         apiClient4.getInidanToons().enqueue(
             object : Callback<IndianToonsModel> {
                 override fun onResponse(
@@ -65,10 +61,9 @@ class KidsExpandFragment : Fragment(R.layout.fragment_kids_expand)  {
     }
 
     private fun setInidanToonsRecycler() {
-        val linearLayoutManager =
-            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
         val indianToon = kidsandfamilytvexpansionAdaptor(indianToon)
         rvkidsexpand.adapter = indianToon
-        rvkidsexpand.layoutManager = linearLayoutManager
+        rvkidsexpand.layoutManager = GridLayoutManager(activity,2)
     }
 }
